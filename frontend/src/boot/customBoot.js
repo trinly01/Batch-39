da7a901f96a7ab15cb6d46877bb350496dbc44d6
Trinmar Boado <timi.boado@gmail.com>
2022-05-02 19:27:19 +0800

    frontend connection using Axios

--- frontend/src/boot/customBoot.js
+++ frontend/src/boot/customBoot.js
@@ -1,7 +1,11 @@
 import VueChartkick from 'vue-chartkick'
 import 'chartkick/chart.js'
+import axios from 'axios'
 
 export default async ({ app }) => {
   // something to do
   app.use(VueChartkick)
+  app.config.globalProperties.$api = axios.create({
+    baseURL: 'http://localhost:3030'
+  })
 }
--- frontend/src/pages/todosPage.vue
+++ frontend/src/pages/todosPage.vue
@@ -17,14 +17,14 @@
     <q-list bordered separator>
       <q-item v-for="(todo, i) in data.todos" :key="todo._id" :class="{ completed: todo.isDone }">
         <q-item-section avatar>
-          <q-checkbox v-model="todo.isDone" />
+          <q-checkbox :modelValue="todo.isDone" @click="update(todo)" />
         </q-item-section>
         <q-item-section v-show="data.changing === i">
           <q-input ref="changeInput" filled v-model="data.change" @blur="data.changing = -1" @keyup.esc="data.changing = -1" @keyup.enter="todo.desc = data.change, data.change = '', data.changing = -1" />
         </q-item-section>
         <q-item-section v-show="data.changing !== i" @dblclick="edit(i)">{{ todo.desc }}</q-item-section>
         <q-item-section side>
-          <q-btn icon="delete" round @click="remove(i)" />
+          <q-btn icon="delete" round @click="remove(i, todo._id)" />
         </q-item-section>
       </q-item>
     </q-list>
@@ -33,12 +33,20 @@
 </template>
 
 <script setup>
-import tProfile from 'components/tProfile.vue'
 
-import { reactive, ref, computed } from 'vue'
+import tProfile from 'components/tProfile.vue'
+import { reactive, ref, computed, getCurrentInstance, onMounted } from 'vue'
 import { Dialog } from 'quasar'
+
 import('pages/style.css')
 const changeInput = ref(null)
+const { appContext: { config: { globalProperties: { $api } } } } = getCurrentInstance()
+
+onMounted(async () => {
+  const { data: { data: todos } } = (await $api.get('/todos'))
+  data.todos = todos.map(todo => ({ ...todo, desc: todo.task }))
+  console.log(data.todos)
+})
 
 const data = reactive({
   task: '',
@@ -50,6 +58,13 @@ const data = reactive({
 const completed = computed(() => data.todos.filter(t => t.isDone).length)
 const active = computed(() => data.todos.length - completed.value)
 
+async function update (todo) {
+  await $api.patch('/todos/' + todo._id, {
+    isDone: !todo.isDone
+  })
+  todo.isDone = !todo.isDone
+}
+
 function chat (name) {
   Dialog.create({
     title: 'Chat',
@@ -57,14 +72,21 @@ function chat (name) {
   })
 }
 
-function add () {
-  data.todos.push({
-    _id: Date.now(),
-    desc: data.task,
-    isDone: false,
-    changing: -1,
-    change: ''
-  })
+async function add () {
+  // data.todos.push({
+  //   _id: Date.now(),
+  //   desc: data.task,
+  //   isDone: false,
+  //   changing: -1,
+  //   change: ''
+  // })
+  const result = (await $api.post('/todos', {
+    task: data.task,
+    isDone: false
+  }))
+  console.log('result', result.data)
+  const todo = result.data
+  data.todos.push({ ...todo, desc: todo.task })
   data.task = ''
 }
 
@@ -86,7 +108,10 @@ function clear () {
   // })
 }
 
-const remove = (index) => data.todos.splice(index, 1)
+const remove = (index, _id) => {
+  $api.delete('/todos/' + _id)
+  data.todos.splice(index, 1)
+}
 
 </script>
 
